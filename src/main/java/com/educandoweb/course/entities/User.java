2a7196298cd4a42f71d4a466d0d6eed89bbaa3be
danilo-sgalvao <danilo.galvao@discente.ufg.br>
2025-11-20 21:49:09 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,6 +3,7 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,6 +30,18 @@ public class User implements Serializable{
 	
 	public User() {
 		}
+	
+	
+
+	public User(Long id, String name, String email, String phone, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+
 
 	public Long getId() {
 		return id;
